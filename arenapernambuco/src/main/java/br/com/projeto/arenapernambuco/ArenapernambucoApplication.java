719e83e4f6853ce3c4fd43131ec20b2e66bc39de
Samara020 <smn@cesar.school>
2026-03-21 13:33:14 -0300

    string boot

---
+++ arenapernambuco/src/main/java/br/com/projeto/arenapernambuco/ArenapernambucoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.arenapernambuco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenapernambucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenapernambucoApplication.class, args);
+	}
+
+}
